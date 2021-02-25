701d2a86abec1881f7a45f98ed509649648d52f8
Justin Benedick <beanisgood@gmail.com>
2021-02-24 20:04:15 -0600

    updated railwarz launchstrings
    updated railwarz launchstrings to autosense the ip address

--- launchstrings/q2-rail-ctf.sh
+++ launchstrings/q2-rail-ctf.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
+ipaddr="$(ifconfig eno1 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')"
+
 cd ~/quake2
-./quake2 +set dedicated 1 +game railwarz +map q2ctf1 +exec server.cfg +rcon_password password +set server_ip "172.30.10.19"
+./quake2 +set dedicated 1 +game railwarz +map q2ctf1 +exec server.cfg +rcon_password password +set server_ip "$ipaddr"
